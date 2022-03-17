b5aebe1041fea856a1eb38ddc41b9ab9796776a1
vasanthi-dev <vasanthi2dev@gmail.com>
2022-03-17 05:28:57 -0400

    subnet name with index

--- subnets.tf
+++ subnets.tf
@@ -1,19 +1,19 @@
-resource "aws_subnet" "public-subnets" {
+resource "aws_subnet" "public-subnet" {
   count      = length(var.PUBLIC_SUBNET_CIDR)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.PUBLIC_SUBNET_CIDR[count.index]
 
   tags = {
-    Name = "public-subnets"
+    Name = "public-subnet-${count.index}"
   }
 }
 
-resource "aws_subnet" "private-subnets" {
+resource "aws_subnet" "private-subnet" {
   count      = length(var.PRIVATE_SUBNET_CIDR)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.PRIVATE_SUBNET_CIDR[count.index]
 
   tags = {
-    Name = "private-subnets"
+    Name = "private-subnet--${count.index}"
   }
 }
