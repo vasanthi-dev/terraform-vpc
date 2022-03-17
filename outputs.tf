76639feddbd1874ea00fd554f97e6dcfa787d370
vasanthi-dev <vasanthi2dev@gmail.com>
2022-03-17 15:21:56 -0400

    vpc outputs

---
+++ outputs.tf
@@ -0,0 +1,24 @@
+output "VPC_ID" {
+  value = aws_vpc.main.id
+}
+
+output "VPC_CIDR" {
+  value = var.VPC_CIDR
+}
+
+output "PUBLIC_SUBNETS" {
+  value = aws_subnet.public-subnet.*.id
+}
+
+output "PRIVATE_SUBNETS" {
+  value = aws_subnet.private-subnet.*.id
+}
+
+output "PUBLIC_SUBNET_CIDR" {
+  value = var.PUBLIC_SUBNET_CIDR
+}
+
+output "PRIVATE_SUBNET_CIDR" {
+  value = var.PRIVATE_SUBNET_CIDR
+}
+
