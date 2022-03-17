65680bf7b515e38927552d0209977605e17fa6c7
vasanthi-dev <vasanthi2dev@gmail.com>
2022-03-17 09:47:19 -0400

    peering connection between default to dev vpc

---
+++ peer.tf
@@ -0,0 +1,10 @@
+resource "aws_vpc_peering_connection" "peer-connection" {
+  peer_owner_id = data.aws_caller_identity.current.account_id
+  peer_vpc_id   = aws_vpc.main.id
+  vpc_id        = data.aws_vpc.default.id
+  auto_accept   = true
+
+  tags = {
+    Name = "VPC Peering between default-${ENV}"
+  }
+}
--- vars.tf
+++ vars.tf
@@ -3,3 +3,4 @@ variable "ENV" {}
 variable "PUBLIC_SUBNET_CIDR" {}
 variable "PRIVATE_SUBNET_CIDR" {}
 variable "AZS" {}
+variable "x" {}
