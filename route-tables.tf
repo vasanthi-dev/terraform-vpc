19b8e85df0bd426bd90397c8e4528ad78ecb3326
vasanthi-dev <vasanthi2dev@gmail.com>
2022-03-17 06:32:48 -0400

    internet gateway route entry added in route table

--- route-tables.tf
+++ route-tables.tf
@@ -27,3 +27,9 @@ resource "aws_route_table_association" "private" {
   subnet_id      = aws_subnet.private-subnet.*.id[count.index]
   route_table_id = aws_route_table.private-table.id
 }
+
+resource "aws_route" "public-igw-route" {
+  route_table_id            = aws_route_table.public-table.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id                = aws_internet_gateway.gw.id
+}
