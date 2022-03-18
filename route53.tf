d474bdae841019bf44c971f57e299783a49c4d66
vasanthi-dev <vasanthi2dev@gmail.com>
2022-03-18 11:38:24 -0400

    frontend

--- route53.tf
+++ route53.tf
@@ -1,4 +1,4 @@
-resource "aws_route53_zone" "example" {
+resource "aws_route53_zone_association" "private-dns" {
   vpc_id  = aws_vpc.main.id
   zone_id = data.aws_route53_zone.privatezone.zone_id
 }
